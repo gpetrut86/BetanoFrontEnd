a7f9be41a27c7d35eae16afeb925e750bcaeebc3
gpetrut86 <gpetrut86@gmail.com>
2023-01-26 16:50:57 +0200

    active class handeling

--- main.js
+++ main.js
@@ -6,6 +6,12 @@ window.onload = function () {
     let closeBtn = document.querySelector('.close-btn');
     let collectBtn = document.querySelector('.popup-section-container-body-btn');
 
+    let standingsNavItems = [...document.querySelectorAll(".standings-section-nav-item")];
+    let standingsFilterItems = [...document.querySelectorAll(".standings-section-filters-item")];
+    let standingsFilterDisplayItems = [...document.querySelectorAll(".standings-section-filters-display-item")];
+    let tabelHamburgers = [document.querySelector(".tabel-hamburger1"), document.querySelector(".tabel-hamburger2")];
+    let head2headFilterItems = [...document.querySelectorAll(".head2head-section-filter-item")];
+    let head2headFilterScrollItems = [...document.querySelectorAll(".head2head-section-filterScroll-item")];
 
     // show popup after 5 seconds
     setTimeout(function () {
@@ -26,6 +32,13 @@ window.onload = function () {
 
     });
 
+    handleActiveElements(standingsNavItems);
+    handleActiveElements(standingsFilterItems);
+    handleActiveElements(standingsFilterDisplayItems);
+    handleActiveElements(tabelHamburgers);
+    handleActiveElements(head2headFilterItems);
+    handleActiveElements(head2headFilterScrollItems);
+
 }
 function hidePopup(element) {
 
@@ -35,4 +48,17 @@ function hidePopup(element) {
 function showPopup(element) {
 
     element.classList.remove('hide');
+}
+
+
+function handleActiveElements(elements) {
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].addEventListener('click', function () {
+
+            for (let j = 0; j < elements.length; j++) {
+                elements[j].classList.remove('active');
+            }
+            elements[i].classList.add('active');
+        });
+    }
 }
